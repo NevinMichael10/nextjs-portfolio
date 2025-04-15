c8f10f355ec514b174322b659bab2aa5b0f16a71
nevinmichael28 <nevin@strebentechnik.com>
2025-04-15 18:40:17 +0530

    removes security

--- next.config.ts
+++ next.config.ts
@@ -1,46 +1,46 @@
 import type { NextConfig } from "next";
 
 const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: `
-      default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval';
-      style-src 'self' 'unsafe-inline';
-      img-src 'self' data:;
-      font-src 'self';
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      connect-src 'self';
-      media-src 'self';
-      frame-ancestors 'none';
-      frame-src vercel.live;
-      block-all-mixed-content;
-      upgrade-insecure-requests;
-      worker-src blob: 'self';
-    `.replace(/\s{2,}/g, " ").trim(),
-  },
-  {
-    key: "Referrer-Policy",
-    value: "strict-origin-when-cross-origin",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "DENY",
-  },
+  // {
+  //   key: "Content-Security-Policy",
+  //   value: `
+  //     default-src 'self';
+  //     script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  //     style-src 'self' 'unsafe-inline';
+  //     img-src 'self' data:;
+  //     font-src 'self';
+  //     object-src 'none';
+  //     base-uri 'self';
+  //     form-action 'self';
+  //     connect-src 'self';
+  //     media-src 'self';
+  //     frame-ancestors 'none';
+  //     frame-src vercel.live;
+  //     block-all-mixed-content;
+  //     upgrade-insecure-requests;
+  //     worker-src blob: 'self';
+  //   `.replace(/\s{2,}/g, " ").trim(),
+  // },
+  // {
+  //   key: "Referrer-Policy",
+  //   value: "strict-origin-when-cross-origin",
+  // },
+  // {
+  //   key: "X-Content-Type-Options",
+  //   value: "nosniff",
+  // },
+  // {
+  //   key: "X-Frame-Options",
+  //   value: "DENY",
+  // },
   {
     key: "X-XSS-Protection",
     value: "1; mode=block",
   },
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=()",
-  },
+  // {
+  //   key: "Permissions-Policy",
+  //   value: "camera=(), microphone=(), geolocation=()",
+  // },
 ];
 
 const nextConfig: NextConfig = {
